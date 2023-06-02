14162ce0fb78c43cd81687bb73a03efd66ef7151
TaranJoh <126112990+TaranJoh@users.noreply.github.com>
2023-06-02 21:56:36 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var bed = {
+  name: "Malm",
+  size: "140x200 cm",
+  colour: "white",
+  numberOfShelves: 2,
+  price: 3759,
+  used: true,
+  made: false,
+  equippedWith: ["Six pillows", "Two blankets", "Bedspread", "Stuffed animal"],
+  makeBed: function () {},
+};
+
+var heading = document.querySelector("h1");
+heading.innerHTML = bed.name;
+
+var button = document.querySelector("button");
