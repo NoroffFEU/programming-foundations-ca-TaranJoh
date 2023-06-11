5aea526d697166ad67c994d086863f916f2fa17e
TaranJ <taran.91@hotmail.com>
2023-06-11 17:11:03 +0200

    updates to styles and functions

--- README.md
+++ README.md
@@ -3,14 +3,12 @@
 This object has the following properties:
 
 - Name (String), the product name of the bed
-- Size (String), the size of the bed
-- Colour (String), the colour of the bed
+- Appearence (nested object property)
+  - Size (String), the size of the bed
+  - Colour (String), the colour of the bed
 - Number of shelves (Number), the number of storage boxes attached to the bed
 - Price (Number), the price it was sold for
 - Used (Boolean), wether the bed has been used or not
 - Made (Boolean), wether the bed is made or not
 - Equppied with (Array), the current items covering the bed
-- Make bed (function)
-
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=11242299&assignment_repo_type=AssignmentRepo)
-# Programming Foundations CA
+- Change colour (function), changes the colour of the bed
--- js/bed.js
+++ js/bed.js
@@ -1,18 +1,44 @@
-var bed = {
+const bed = {
   name: "Malm",
-  size: "140x200 cm",
-  colour: "white",
+  appearence: {
+    size: "140x200 cm",
+    colour: "white",
+  },
   numberOfShelves: 2,
   price: 3759,
-  used: true,
-  made: false,
+  used: false,
+  made: true,
   equippedWith: ["Six pillows", "Two blankets", "Bedspread", "Stuffed animal"],
-  makeBed = function(){}
+  changeColour: function () {
+    image.src = "/images/eikefiner.avif";
+    bed.appearence.colour = "oak";
+  },
+};
+
+// Declaring variables from html
+var button = document.querySelector("button");
+var image = document.querySelector("img");
+var heading = document.querySelector("h1");
+var colour = document.querySelector("#colour");
+var size = document.querySelector("#size");
+var price = document.querySelector("#price");
+var used = document.querySelector("#used");
+var shelves = document.querySelector("#shelves");
+
+heading.innerHTML = bed.name;
+colour.innerHTML = bed.appearence.colour;
+size.innerHTML = bed.appearence.size;
+price.innerHTML = bed.price + ",-";
+shelves.innerHTML = bed.numberOfShelves;
+
+function bedUsed() {
+  if (bed.used === false) {
+    used.innerHTML = "new";
+  } else {
+    used.innerHTML = "used";
+  }
 }
 
-
-
-var heading = document.querySelector("h1");
-heading.innerHTML = bed.name;
-
-var button = document.querySelector("button");
+// Calling function
+button.onclick = bed.changeColour;
+bedUsed();
